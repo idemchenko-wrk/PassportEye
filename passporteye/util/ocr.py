75579bc5ec87ad49034d5a67855d9014941d1193
Ilya Demchenko <idemchenko.wrk@yandex.ru>
2019-12-15 19:02:05 +0300

    remove tabs

--- passporteye/util/ocr.py
+++ passporteye/util/ocr.py
@@ -40,7 +40,7 @@ def ocr(img, mrz_mode=True, extra_cmdline_params=''):
         imwrite(input_file_name, img)
 
         if mrz_mode:
-			# NB: Tesseract 4.0 does not seem to support tessedit_char_whitelist
+            # NB: Tesseract 4.0 does not seem to support tessedit_char_whitelist
             config = ("--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><"
                       " -c load_system_dawg=F -c load_freq_dawg=F {}").format(extra_cmdline_params)
         else:
